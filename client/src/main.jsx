0ee40f1ffdb0cce46e880d2d45bc14a2901a647c
Ketoran Sileshi <abdisileshi123@gmail.com>
2024-09-14 20:50:12 +0300

    auth0 updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         domain="dev-3u03a67skwnv6f7u.us.auth0.com"
         clientId="WQlNnOUMn8WMUfLyRLQCfprzyTiWZI5U"
         authorizationParams={{
-          redirect_uri: "http://localhost:5173",
+          redirect_uri: "https://bet-real-estate-project-pi.vercel.app",
         }}
         audience="http://localhost:8000"
         scope="openid profile email"
